538954e6e78bc821ce8204e31de5b77dd0764d98
Skcshivam <shichoudhary999@gmail.com>
2025-01-05 01:24:50 +0530

    react router

--- 07reactRouter/src/components/Github/Github.jsx
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Github() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/mandliyarajendra11`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
+
   return (
     <>
       <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
-        GIthub Follower :
+        GIthub Follower :{data.followers}
+        <img src={data.avatar_url} alt="" />
       </div>
     </>
   );
